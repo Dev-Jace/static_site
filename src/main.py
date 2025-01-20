02cb0a20632b5e8fbc1ecab13ff9a1824c30b275
Dev-Jace <prescott.chris.lee@gmail.com>
2025-01-20 13:06:02 -0800

    cleaned up main

--- src/main.py
+++ src/main.py
@@ -1,31 +1,6 @@
 from textnode import *
 import os , shutil
-''' # 
-def refresh_public2(dest_dir,retrieve_dir):
-    shutil.rmtree(dest_dir,ignore_errors=True)
-    os.mkdir(dest_dir)
 
-    files_2_copy = os.listdir(retrieve_dir)
-
-    for file in files_2_copy:
-        if not os.path.isdir(file):
-            copy_file()
-            shutil.copy(file, dest_dir)
-    
-def copy_file2(retrieve_path, dest):
-    files_list = []
-    directory = os.listdir(retrieve_path)
-    #print("~",directory)
-    
-    for file in directory:
-        #print("~",directory,"~",file,":",not os.path.isdir(retrieve_path + file))
-        if not os.path.isdir(retrieve_path + file):
-            files_list.append(retrieve_path + file)
-        else:
-            files_list.extend(file_paths(retrieve_path + file + "/"))
-
-    return 
-'''
 def refresh_public(retrieve_dir, dest_dir):
     shutil.rmtree(dest_dir,ignore_errors=True)
     os.mkdir(dest_dir)
@@ -42,8 +17,8 @@ def refresh_public(retrieve_dir, dest_dir):
 
 def main():
     tester = TextNode("This is a text node", "bold", "https://www.boot.dev")
-    dest_dir = "../public/"
-    retrieve_dir = "../static/"
+    dest_dir = "./public/"
+    retrieve_dir = "./static/"
     
     refresh_public(retrieve_dir, dest_dir)
 
